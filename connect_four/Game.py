f153148800f5ee9304767831103d7fc9fbe6e6dd
Jahn Thomas Fidje <jtfidje@gmail.com>
2016-11-24 20:26:17 +0100

    Added new heuristics function and converted tabs to spaces

--- connect_four/Game.py
+++ connect_four/Game.py
@@ -7,84 +7,91 @@ PLAYER_2 = 'O'
 
 
 class Game:
-	def __init__(self):
-		self.winner = None
-		self.create_board()
- 
+    def __init__(self):
+        self.winner = None
+        self.create_board()
 
-	def create_board(self):
-		self.board = [[BOARD for _ in range(ROWS)] for _ in range(COLS)]
+        self.score_table = {
+            (BOARD, BOARD, BOARD, PLAYER_1): 1/4,
+            (BOARD, BOARD, PLAYER_1, PLAYER_1): 2/4,
+            (BOARD, PLAYER_1, PLAYER_1, PLAYER_1): 3/4,
+        }
 
+    def create_board(self):
+        self.board = [[BOARD for _ in range(ROWS)] for _ in range(COLS)]
 
-	def put_piece(self, col, piece):
-		try:
-			row = self.board[col].index(BOARD)
-			self.board[col][row] = piece
-			return True
-		except (ValueError, IndexError):
-			return False
+    def put_piece(self, col, piece):
+        try:
+            row = self.board[col].index(BOARD)
+            self.board[col][row] = piece
+            return True
+        except (ValueError, IndexError):
+            return False
 
+    def check_winner(self, board=None):
+        self.max_score = 0
 
-	def check_winner(self, board=None):
-		if not board:
-			board = self.board
+        if not board:
+            board = self.board
 
-		# Check draw
-		if BOARD not in [col[-1] for col in board]:
-			self.winner = 'Draw'
-			return True
+        # Check draw
+        if BOARD not in [col[-1] for col in board]:
+            self.winner = 'Draw'
+            return True
 
-		# Check horizontal
-		for row in range(ROWS):
-			for col in range(COLS - 3):
-				window = []
-				for i in range(col, col + 4):
-					window.append(board[i][row])
-				if self.check_window(window):
-					return True
-				
-		# Check vertical
-		for col in range(COLS):
-			for i in range(ROWS - 3):
-				window = board[col][i:i + 4]
-				if self.check_window(window):
-					return True
-		
-		# Check diagonal
-		for col in range(COLS):
-			for row in range(ROWS):
-				if col + 3 < COLS and row < ROWS - 3:
-					window = [board[col + i][row + i] for i in range(4)] 
-					if self.check_window(window):
-						return True
+        # Check horizontal
+        for row in range(ROWS):
+            for col in range(COLS - 3):
+                window = []
+                for i in range(col, col + 4):
+                    window.append(board[i][row])
+                if self.check_window(window):
+                    return True
 
-				if row >= 3 and col < COLS - 3:
-					window = [board[col + i][row - i] for i in range(4)]
-					if self.check_window(window):
-						return True
+        # Check vertical
+        for col in range(COLS):
+            for i in range(ROWS - 3):
+                window = board[col][i:i + 4]
+                if self.check_window(window):
+                    return True
 
-		return False
+        # Check diagonal
+        for col in range(COLS):
+            for row in range(ROWS):
+                if col + 3 < COLS and row < ROWS - 3:
+                    window = [board[col + i][row + i] for i in range(4)]
+                    if self.check_window(window):
+                        return True
 
+                if row >= 3 and col < COLS - 3:
+                    window = [board[col + i][row - i] for i in range(4)]
+                    if self.check_window(window):
+                        return True
+        return False
 
-	def check_window(self, window):
-		mark = window[0] if window[0] != BOARD else None
-		if all(i == mark for i in window):
-			self.winner = mark
-			return True
-		return False
+    def check_window(self, window):
+        mark = window[0] if window[0] != BOARD else None
+        if all(i == mark for i in window):
+            self.winner = mark
+            return True
+        window.sort()
+        i = self.score_table.get(tuple(window), 0)
+        if i > self.max_score:
+            self.max_score = i
 
+        return False
 
-	def __repr__(self):
-		return (
-			'\n'
-			.join([''
-			.join(['{:3}'
-			.format(self.board[col][row])
-			for col in range(COLS)]) 
-			for row in range(ROWS - 1, -1, -1)
-			]))
+    def __repr__(self):
+        return (
+            '\n'
+            .join([''
+            .join(['{:3}'
+            .format(self.board[col][row])
+            for col in range(COLS)])
+            for row in range(ROWS - 1, -1, -1)
+            ]))
 
 if __name__ == '__main__':
-	game = Game()
-	
-	print(game)
+    game = Game()
+
+    print(game)
--- connect_four/main.py
+++ connect_four/main.py
@@ -7,145 +7,151 @@ from Game import Game, COLS, ROWS, PLAYER_1, PLAYER_2
 
 game = Game()
 
+
 def print_ending(game):
-	if game.winner == 'Draw':
-		print('\nGame ended in draw!')
-	else:
-		print('\n{} won the game!'.format(game.winner))
+    if game.winner == 'Draw':
+        print('\nGame ended in draw!')
+    else:
+        print('\n{} won the game!'.format(game.winner))
+
 
 def print_game(game):
-	os.system('clear')
-	print(game)
-	time.sleep(0.2)
+    os.system('clear')
+    print(game)
+    time.sleep(0.2)
+
 
 def random_play():
-	game.__init__()
-	i = 0
-	while not game.check_winner():
-		i += 1
+    game.__init__()
+    i = 0
+    while not game.check_winner():
+        i += 1
 
-		player = PLAYER_1 if i % 2 else PLAYER_2
+        player = PLAYER_1 if i % 2 else PLAYER_2
 
-		# Make player move
-		while not game.put_piece(random.randrange(0, COLS), player):
-			pass
-		
-		print_game(game)
+        # Make player move
+        while not game.put_piece(random.randrange(0, COLS), player):
+            pass
 
-	print_ending(game)
+        print_game(game)
+
+    print_ending(game)
+
 
 def pvp():
-	game.__init__()
-	i = 0
+    game.__init__()
+    i = 0
 
-	print(game)
-	while not game.check_winner():
-		i += 1
+    print(game)
+    while not game.check_winner():
+        i += 1
 
-		player = PLAYER_1 if i % 2 else PLAYER_2
+        player = PLAYER_1 if i % 2 else PLAYER_2
 
-		# Make player move
-		while True:
-			try:
-				move = int(input('\n{} - Enter col:'.format(player))) - 1
-			except ValueError:
-				continue
-			except EOFError:
-				import sys; sys.exit()
+        # Make player move
+        while True:
+            try:
+                move = int(input('\n{} - Enter col:'.format(player))) - 1
+            except ValueError:
+                continue
+            except EOFError:
+                import sys; sys.exit()
 
-			if game.put_piece(move, player):
-				break
-		
-		print_game(game)
+            if game.put_piece(move, player):
+                break
 
-	print_ending(game)
+        print_game(game)
+
+    print_ending(game)
+
 
 def pve():
-	ai = AI(PLAYER_1, PLAYER_2)
-	i = 0
-	game.__init__()
+    ai = AI(PLAYER_1, PLAYER_2)
+    i = 0
+    game.__init__()
 
-	while not game.check_winner():
-		i += 1
+    while not game.check_winner():
+        i += 1
 
-		if i % 2:
-			# TODO: Get AI turn
-			# player = PLAYER_1
-			# move =
-			move = ai.search(game)
-			player = PLAYER_1
-			game.put_piece(move, PLAYER_1)
+        if i % 2:
+            # TODO: Get AI turn
+            # player = PLAYER_1
+            # move =
+            move = ai.search(game)
+            player = PLAYER_1
+            game.put_piece(move, PLAYER_1)
 
-		else:
-			# Make player move
-			while True:
-				try:
-					move = int(input('\n{} - Enter col:'.format(PLAYER_2))) - 1
-				except ValueError:
-					continue
-				except EOFError:
-					import sys; sys.exit()
+        else:
+            # Make player move
+            while True:
+                try:
+                    move = int(input('\n{} - Enter col:'.format(PLAYER_2))) - 1
+                except ValueError:
+                    continue
+                except EOFError:
+                    import sys; sys.exit()
 
-				if game.put_piece(move, PLAYER_2):
-					break
-		
-		print_game(game)
-		print(ai.pred)
+                if game.put_piece(move, PLAYER_2):
+                    break
 
-	print_ending(game)
+        print_game(game)
+        print(ai.pred)
+
+    print_ending(game)
 
 
 def ave():
-	ai = AI(PLAYER_1, PLAYER_2)
-	i = 0
-	game.__init__()
+    ai = AI(PLAYER_1, PLAYER_2)
+    i = 0
+    game.__init__()
 
-	while not game.check_winner():
-		i += 1
+    while not game.check_winner():
+        i += 1
 
-		if i % 2:
-			# TODO: Get AI turn
-			# player = PLAYER_1
-			# move =
-			move = ai.search(game)
-			player = PLAYER_1
-			game.put_piece(move, PLAYER_1)
+        if i % 2:
+            # TODO: Get AI turn
+            # player = PLAYER_1
+            # move =
+            move = ai.search(game)
+            player = PLAYER_1
+            game.put_piece(move, PLAYER_1)
 
-		else:
-			while not game.put_piece(random.randrange(0, COLS), PLAYER_2):
-				pass
-		
-		print_game(game)
-		print(ai.pred)
+        else:
+            while not game.put_piece(random.randrange(0, COLS), PLAYER_2):
+                pass
 
-	print_ending(game)
+        print_game(game)
+        print(ai.pred)
+
+    print_ending(game)
+
 
 def ava():
-	ai_1 = AI(PLAYER_1, PLAYER_2, max_depth=5)
-	ai_2 = AI(PLAYER_2, PLAYER_1, max_depth=2)
-	i = 0
-	game.__init__()
+    ai_1 = AI(PLAYER_1, PLAYER_2, max_depth=5)
+    ai_2 = AI(PLAYER_2, PLAYER_1, max_depth=2)
+    i = 0
+    game.__init__()
 
-	while not game.check_winner():
-		i += 1
+    while not game.check_winner():
+        i += 1
 
-		if i % 2:
-			# TODO: Get AI turn
-			# player = PLAYER_1
-			# move =
-			move = ai_1.search(game)
-			game.put_piece(move, PLAYER_1)
-		else:
-			move = ai_2.search(game)
-			game.put_piece(move, PLAYER_2)
-		
-		print_game(game)
+        if i % 2:
+            # TODO: Get AI turn
+            # player = PLAYER_1
+            # move =
+            move = ai_1.search(game)
+            game.put_piece(move, PLAYER_1)
+        else:
+            move = ai_2.search(game)
+            game.put_piece(move, PLAYER_2)
 
-	print_ending(game)
+        print_game(game)
+
+    print_ending(game)
 
 
 if __name__ == '__main__':
 #	ave()
 #	pvp()
-	pve()
+    pve()
 #	ava()
